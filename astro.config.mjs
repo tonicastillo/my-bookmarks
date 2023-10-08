23a53989e8b615744d932eedee7b8357b0604803
tonicastillo <tonicastillo@gmail.com>
2023-10-08 20:57:50 +0200

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 
 import react from "@astrojs/react";
+import vercelEdge from "@astrojs/vercel/edge"
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercelEdge(),
   integrations: [react()]
 });
