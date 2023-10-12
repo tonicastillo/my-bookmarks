a33a20535ae992c3240b492e945c48df03ccbe90
tonicastillo <tonicastillo@gmail.com>
2023-10-12 16:08:36 +0200

    .

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,13 @@ read answer
 
 if [ "$answer" = "y" ]; then
   echo "Updating the image..."
-  git pull
+  git pull origin main
+  docker-compose down
   docker build --no-cache -t mybookmarks -f Dockerfile .
   docker-compose up -d --force-recreate
+  docker rmi $(docker images 'mybookmarks' -a -q) -f
 else
   echo "The image will not be updated."
 fi
+
 
